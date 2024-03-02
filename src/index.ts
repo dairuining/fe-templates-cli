b7ff50c4f843cd0233b610a89fb27f925597be22
dairuining <2436480281@qq.com>
2024-03-02 12:32:49 +0800

    feat: 实现命令行version功能

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import { Command } from 'commander';
+import { version } from '../package.json';
+
+const program = new Command();
+
+// 实现version命令
+program.version(version, '-v --version');
+
+/** 解析命令行参数 */
+program.parse(process.argv);
